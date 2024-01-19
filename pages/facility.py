761372f975b5bce6c24b3bff9db9c2b214d55c16
Rohit Vadali <vadalirohit@gmail.com>
2024-01-19 12:46:58 -0800

    Add resident form

--- pages/facility.py
+++ pages/facility.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
+import requests
 
 # General data settings 
 
@@ -20,7 +21,17 @@ st.title(page_title + " " + page_icon)
 
 selected = option_menu(
     menu_title=None,
-    options=["Data Entry", "Data Visualization"],
-    icons=["pencil-fill", "bar-chart-fill"],  # https://icons.getbootstrap.com/
+    options=["Add Resident", "Data Visualization"],
+    # icons=["pencil-fill", "bar-chart-fill"],  # https://icons.getbootstrap.com/
     orientation="horizontal",
 )
+
+if selected == "Add Resident":
+    with st.form("Resident form", clear_on_submit=True):
+        for field in resident_fields:
+            st.text_input(f"{field}:", key=field)
+        submitted = st.form_submit_button("save resident")
+        if submitted:
+            fields = {field: st.session_state[field] for field in resident_fields}
+            print(fields)
+            st.success("Data saved!")
